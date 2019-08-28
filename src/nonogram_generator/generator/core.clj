72f2f4a69c9a8c9c36a3f6ab762c7a1a0330cc73
Daniel Mason <dan@juxt.pro>
2019-08-28 16:40:44 +0100

    Add image loading and start nonogram processing

--- src/nonogram_generator/generator/core.clj
+++ src/nonogram_generator/generator/core.clj
@@ -12,3 +12,48 @@
 (defmethod ig/init-key ::string
   [_ x]
   (string-resource x))
+
+
+(defn load-image [image width height]
+  (-> image
+      (u/mat-from-url CV_8UC1)
+      (resize! (new-size width height))))
+
+(defn binarize-image [image]
+  (-> image
+      (threshold! 80.0 1.0 THRESH_BINARY_INV)
+      ;(adaptive-threshold! 1.0 ADAPTIVE_THRESH_MEAN_C THRESH_BINARY_INV 3 4)
+      ))
+
+(defn get-image-array [image width height]
+  (loop [x 0 img-array []]
+    (if (= width x)
+      img-array
+      (recur
+       (inc x)
+       (conj img-array
+             (loop [y 0 row-array []]
+               (if (= height y)
+                 row-array
+                 (recur
+                  (inc y)
+                  (conj row-array (first (.get image x y)))))))))))
+
+(defn count-row [line]
+  (loop [orig line split []]
+    (cond
+      (empty? orig) (map count split)
+      (= 0.0  (first orig)) (recur (drop-while #(= % 0.0) orig) split)
+      :else
+      (let [zs (split-with #(= % 1.0) orig)]
+        (recur (second zs) (conj split (first zs)))))))
+
+(defn process-image [image width height]
+  (let [xs (byte-array [])]
+    (-> image
+        (load-image width height)
+        (binarize-image)
+        (get-image-array width height)
+        ;(#(map count-row %))
+        ;(#(map count-row (apply mapv vector %)))
+        )))
