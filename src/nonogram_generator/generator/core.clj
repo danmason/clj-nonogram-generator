dbd476f012cf3fd0c46e7b01b4f16ffffa8895f3
Daniel Mason <dan@juxt.pro>
2019-08-28 16:38:03 +0100

    Add image processing tools to the project via origami opencv wrapper

--- src/nonogram_generator/generator/core.clj
+++ src/nonogram_generator/generator/core.clj
@@ -1,7 +1,9 @@
 (ns nonogram-generator.generator.core
   (:require
     [yada.yada :as yada]
-    [integrant.core :as ig]))
+    [integrant.core :as ig]
+    [opencv4.utils :as u]
+    [opencv4.core :refer :all]))
 
 (defn string-resource
   [x]
