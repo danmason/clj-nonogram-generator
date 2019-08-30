e670d2c3508c2a57a47e576ec105e8256d6e1b6a
Daniel Mason <dan@juxt.pro>
2019-08-30 14:48:24 +0100

    Exclude conflicting functions from origami

--- src/nonogram_generator/generator/core.clj
+++ src/nonogram_generator/generator/core.clj
@@ -4,7 +4,8 @@
    [yada.yada :as yada]
    [integrant.core :as ig]
    [opencv4.utils :as u]
-   [opencv4.core :refer :all]))
+   [opencv4.core :refer :all :exclude [min reduce sort merge
+                                       compare repeat max]]))
 
 (defn string-resource
   [x]
